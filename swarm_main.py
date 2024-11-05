fe2c0cf6eb3d6b5fa8750851a5ed9196d4f0628c
hoang-danny05 <bajanboss132@gmail.com>
2024-11-05 12:37:21 -0700

    changed filenames to make them valid test

--- swarm_main.py
+++ swarm_main.py
@@ -12,7 +12,7 @@ from utils.enums import ModelType
 
 model = ModelType.GPT_3_5_TURBO
 version = "v0.4"
-filename = datetime.now().strftime(f"{version} %m-%d-%Y at %H:%M.json")
+filename = datetime.now().strftime(f"{version}_%m_%d_%Y_at_%H;%M.json")
 filename = f"Warehouse/{filename}"
 ###############################################################################################
 # AGENT DEFINITIONS (base definitions)
