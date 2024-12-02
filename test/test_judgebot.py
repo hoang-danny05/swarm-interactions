09a5bfd3e01ade89833b9a0b78a78459e20fd999
hoang-danny05 <bajanboss132@gmail.com>
2024-12-02 08:57:09 -0700

    made my first pytest module

--- test/test_judgebot.py
+++ test/test_judgebot.py
@@ -1,3 +1,4 @@
+#intended to be run with pytest.
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from customname.JudgeBot import doJudgement
@@ -5,11 +6,10 @@ from utils.file_reader import get_messages_from
 from utils.output import pretty_print_messages
 from typing import List
 
-# pretty_print_messages(messages=messages)
 outcome_a = "formal day selected"
 outcome_b = "pajama day selected"
 
-state = [0,0,0];
+state = [0,0,0]
 
 def formal_selected():
     state[0] = 1
@@ -20,45 +20,50 @@ def pj_selected():
 def nothing_selected():
     state[2] = 1
 
+def reset_state():
+    state[0] = 0
+    state[1] = 0
+    state[2] = 0
 
 # test pajama day
-messages = get_messages_from("./test/pajama_day.json")
-doJudgement(messages=messages, 
-            outcome_a=outcome_a,
-            outcome_b=outcome_b,
-            on_outcome_a=formal_selected,
-            on_outcome_b=pj_selected,
-            on_neutral_outcome=nothing_selected)
+def test_pajama_day():
+    reset_state()
+    messages = get_messages_from("./test/pajama_day.json")
+    doJudgement(messages=messages, 
+                outcome_a=outcome_a,
+                outcome_b=outcome_b,
+                on_outcome_a=formal_selected,
+                on_outcome_b=pj_selected,
+                on_neutral_outcome=nothing_selected)
 
-print(state)
+    print(state)
 
-assert [0,1,0] == state, "Pajama day not corretly selected. Exiting."
+    assert [0,1,0] == state, "Pajama day not corretly selected. Exiting."
 
 #test formal day
-state = [0,0,0];
-messages = get_messages_from("./test/formal_day.json")
-doJudgement(messages=messages, 
-            outcome_a=outcome_a,
-            outcome_b=outcome_b,
-            on_outcome_a=formal_selected,
-            on_outcome_b=pj_selected,
-            on_neutral_outcome=nothing_selected)
+def test_formal_day():
+    reset_state()
+    messages = get_messages_from("./test/formal_day.json")
+    doJudgement(messages=messages, 
+                outcome_a=outcome_a,
+                outcome_b=outcome_b,
+                on_outcome_a=formal_selected,
+                on_outcome_b=pj_selected,
+                on_neutral_outcome=nothing_selected)
 
-assert [1,0,0] == state, "Formal day not corretly selected. Exiting."
+    assert [1,0,0] == state, "Formal day not corretly selected. Exiting."
 
 #test no resolution
-state = [0,0,0];
-messages = get_messages_from("./test/no_resolution.json")
-doJudgement(messages=messages, 
-            outcome_a=outcome_a,
-            outcome_b=outcome_b,
-            on_outcome_a=formal_selected,
-            on_outcome_b=pj_selected,
-            on_neutral_outcome=nothing_selected)
+def test_no_resolution():
+    reset_state()
+    messages = get_messages_from("./test/no_resolution.json")
+    doJudgement(messages=messages, 
+                outcome_a=outcome_a,
+                outcome_b=outcome_b,
+                on_outcome_a=formal_selected,
+                on_outcome_b=pj_selected,
+                on_neutral_outcome=nothing_selected)
 
-assert [0,0,1] == state, "Formal day not corretly selected. Exiting."
+    assert [0,0,1] == state, "Formal day not corretly selected. Exiting."
 
-
-
-
-print("The file successfuly ran without error!")
+raise Exception("Not meant to be run as a main script! use pytest instead!")
