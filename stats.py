914cf058602dbf5183f3b28f3464180ba0ad3641
aka-hombre <148398172+aka-hombre@users.noreply.github.com>
2025-02-19 19:52:04 -0700

    script to perform chi square test for independence

---
+++ stats.py
@@ -0,0 +1,28 @@
+import scipy.stats as stats
+import os
+import json
+import numpy as np
+
+def chitestNprint(observed):
+    chi2_stat, p_value, dof, expected = stats.chi2_contingency(observed)
+    
+    print(f"Chi-square Statistic: {chi2_stat:.4f}")
+    print(f"P-value: {p_value:.4f}")
+    print(f"Degrees of Freedom: {dof}")
+    print("Expected Frequencies:")
+    print(np.round(expected, 3)) 
+
+    return chi2_stat, p_value, dof, expected
+
+filenames = ["./Warehouse/BB/results_run4o.json","./Warehouse/BA/results_run4o.json",
+             "./Warehouse/AB/results_run4o.json","./Warehouse/AA/results_run4o.json"]
+
+observations = np.empty((4,4))
+for i, name in enumerate(filenames):
+    data = json.load(open(name))
+    observations[i,:] = [data["PajamaWins"], data["FormalWins"], 
+                         data["NoWins"], data["ConfusedIdentity"]]
+
+print(observations)
+
+chi2_stat, p_value, dof, expected = chitestNprint(observations)
