e6578010a652b85b64cfa6bc9e7c6605f010a5d4
aka-hombre <iarodr13@asu.edu>
2025-06-28 10:37:30 -0700

    removed some old code

--- stats.py
+++ stats.py
@@ -2,30 +2,6 @@ import os
 import json
 import numpy as np
 import pandas as pd
-'''
-def chitestNprint(observed):
-    chi2_stat, p_value, dof, expected = stats.chi2_contingency(observed)
-    
-    print(f"Chi-square Statistic: {chi2_stat:.4f}")
-    print(f"P-value: {p_value:.4f}")
-    print(f"Degrees of Freedom: {dof}")
-    print("Expected Frequencies:")
-    print(np.round(expected, 3)) 
-
-    return chi2_stat, p_value, dof, expected
-
-filenames = ["./Warehouse/BB/results_run4o.json","./Warehouse/BA/results_run4o.json",
-             "./Warehouse/AB/results_run4o.json","./Warehouse/AA/results_run4o.json"]
-
-observations = np.empty((4,3))
-for i, name in enumerate(filenames):
-    data = json.load(open(name))
-    observations[i,:] = [data["PajamaWins"], data["FormalWins"], data['NoWins']]
-
-print(observations)
-
-chi2_stat, p_value, dof, expected = chitestNprint(np.transpose(observations)) 
-'''
 
 def resultsDF(directory, filename, tocsv=False):
     """
