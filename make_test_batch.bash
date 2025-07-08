c4e379df27d2617532e2f02d08334b94daca3f0f
aka-hombre <iarodr13@asu.edu>
2025-07-08 12:20:02 -0700

    modified bash to make a copy of configuration directories to later move sample of files to

--- make_test_batch.bash
+++ make_test_batch.bash
@@ -1,8 +1,10 @@
 # !bin/bash
 
-
-for file in $(find Warehouse/ -maxdepth 2 -name "results*"); do
-    name=$(echo $file | grep -o '[A-Z]\{2\}') # extracts configuration case
-
-    echo "$name" # prints number of run in each match up
+find Warehouse/ -mindepth 1 -maxdepth 1 -type d -name '[A-Z][A-Z]' | head -n 36 | while read -r dir; do
+    config=$(echo $dir | grep -o '[A-Z]\{2\}') 
+    mkdir "Warehouse/testing/$config"
+    #echo "$dir"
+    #echo $config
+    #files=$(find $dir -maxdepth 1 -type f -name "run4o*" | shuf -n 5)
+    #echo $files
 done
