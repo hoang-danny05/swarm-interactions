a7935f1867104861e00bf6c7189b1465089097b4
aka-hombre <iarodr13@asu.edu>
2025-07-08 12:48:38 -0700

    throwaway script to sample and log files from main batches in Warehouse/

--- make_test_batch.bash
+++ make_test_batch.bash
@@ -1,10 +1,12 @@
 # !bin/bash
 
+touch sampled_files.txt
 find Warehouse/ -mindepth 1 -maxdepth 1 -type d -name '[A-Z][A-Z]' | head -n 36 | while read -r dir; do
     config=$(echo $dir | grep -o '[A-Z]\{2\}') 
-    mkdir "Warehouse/testing/$config"
-    #echo "$dir"
-    #echo $config
-    #files=$(find $dir -maxdepth 1 -type f -name "run4o*" | shuf -n 5)
-    #echo $files
+    find $dir -type f -name "run4o*" | shuf -n 5 | while read -r file; do
+        echo $file >> sampled_files.txt
+        name=$(echo $file | sed -n 's/.*\(run4o.*\)/\1/p')
+        cp "$file" "Warehouse/testing/$config" 
+    done
+
 done
