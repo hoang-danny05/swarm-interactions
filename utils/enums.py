c4be0fd64223ea0d6b91dadd8f3a896d07450f4e
aka-hombre <iarodr13@asu.edu>
2025-07-14 11:19:36 -0700

    current state during 7/14/2025

--- utils/enums.py
+++ utils/enums.py
@@ -16,7 +16,7 @@ class ExactModelType(Enum):
     GPT_4O_MINI = "gpt-4o-mini-2024-07-18"
     O1 = "o1-2024-12-17"
     O1_MINI = "o1-mini-2024-09-12"
-    O3_MINI = "o3-mini-2025-01-31"
+    O3_MINI = "o3-mini"
 
 class StringEnum(str, Enum):
     def _generate_next_value_(name, start, count, last_values):
