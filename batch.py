4a5be0468e049284890aa513693a86d2ad0adbaa
Danny Hoang <bajanboss132@gmail.com>
2025-07-24 18:18:44 -0700

    finished the judges for runs AA - FF without AA-BB

--- batch.py
+++ batch.py
@@ -2,11 +2,14 @@
 import itertools
 import subprocess
 import traceback
+
 """
 executes a batch of assertiveness observers using the given count.
 
 now does judgements!
 """
+
+subprocess.run(["ls"], shell=True)
 
 possible_slots = "ABCDEF"
 for comb in itertools.product(possible_slots, possible_slots):
@@ -16,9 +19,10 @@ for comb in itertools.product(possible_slots, possible_slots):
         continue
     
     try:
-        print(f"STARTING CONFIG WITH {config}\n\n\n\n\n")
+        # print(f"STARTING CONFIG WITH {config}\n\n\n\n\n")
+        pass
         subprocess.run(
-            ["./get_classifications.py", config, "run4o", "y"],
+            ["python", "get_classifications.py", config, "run4o", "y"],
         )
     except Exception:
         traceback.print_exc()
